2c1c24cc2391c95972636291668a1a6f61f4a9a0
Serghei_Granici <granici.design@gmail.com>
2026-04-13 11:27:12 +0200

    Added to Google exextensions

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div style={{maxWidth: '480px', margin: '40px auto', width: '100%'}}>
+    <div style={{maxWidth: '480px', margin: '40px auto', width: '100%', minWidth: '320px'}}>
       <TodoForm />
       <TodoList />
     </div>
